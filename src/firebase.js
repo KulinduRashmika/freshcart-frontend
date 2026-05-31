9891555c35ce834fc455292d230762c041ed4be9
KulinduRashmika <kulindurashmika5@gmail.com>
2026-05-31 17:07:02 +0530

    update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,9 @@
 import { initializeApp } from "firebase/app";
-
-import {
-  getAuth,
-  GoogleAuthProvider,
-  FacebookAuthProvider,
-} from "firebase/auth";
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA5l5EHf_w_nJJFotGMG0rSuoLjUaKmvgg",
-  authDomain: "shopping-cart-app-4f1d7.firebaseapp.com",
+  authDomain: "freshcart-frontend-pttt.vercel.app",  // ← change to your Vercel URL
   projectId: "shopping-cart-app-4f1d7",
   storageBucket: "shopping-cart-app-4f1d7.firebasestorage.app",
   messagingSenderId: "136998807166",
@@ -18,9 +13,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-
-export const googleProvider =
-  new GoogleAuthProvider();
-
-export const facebookProvider =
-  new FacebookAuthProvider();
+export const googleProvider = new GoogleAuthProvider();
+export const facebookProvider = new FacebookAuthProvider();
